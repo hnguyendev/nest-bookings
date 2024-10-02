645e157f2ebf4a797dffe19d035c09e6028f6b02
hnguyendev <duchung682003@gmail.com>
2024-10-02 23:57:58 +0700

    Add auth service

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
-import { ReservationsModule } from './reservations.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReservationsModule);
+  const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
   app.useLogger(app.get(Logger));
+
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return user;
+  }
+}
